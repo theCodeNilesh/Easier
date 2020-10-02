33422b1e0382d0c35551b5caa8ac03169070aa99
theCodeNilesh <61627365+theCodeNilesh@users.noreply.github.com>
2020-10-02 19:02:16 +0530

    Splash screen is implemented

--- app/src/main/java/com/revolutioncoders/easier/MainActivity.java
+++ app/src/main/java/com/revolutioncoders/easier/MainActivity.java
@@ -1,8 +1,14 @@
 package com.revolutioncoders.easier;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView easier;
+
+        easier = (TextView) findViewById(R.id.easier);
+
+
+        //Text Animation
+
+        easier.setAlpha((float) 0.0);
+
+        easier.animate().alpha((float) 1.0).setDuration(400).setStartDelay(250);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController insetsController = getWindow().getInsetsController();
+            if (insetsController != null) {
+                insetsController.hide(WindowInsets.Type.statusBars());
+            }
+        } else {
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+            );
+        }
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(1300);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i = new Intent(getBaseContext(),
+                            HomeScreen.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
     }
 }
