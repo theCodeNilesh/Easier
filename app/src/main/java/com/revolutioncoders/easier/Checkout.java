ed002b3b3f8cadf60729913e863d78b733a5fdf3
theCodeNilesh <61627365+theCodeNilesh@users.noreply.github.com>
2020-11-04 13:03:09 +0530

    ThankYou screen is implemented

--- app/src/main/java/com/revolutioncoders/easier/Checkout.java
+++ app/src/main/java/com/revolutioncoders/easier/Checkout.java
@@ -1,14 +1,33 @@
 package com.revolutioncoders.easier;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Checkout extends AppCompatActivity {
 
+    Button proceed_btn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        proceed_btn = findViewById(R.id.proceed_btn);
+
+
+        proceed_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Checkout.this, ThankYou.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
